fe6c55e507a921f31a6bb6b1d6e361a31f05d345
symc-dlp-cloud-connector <DL-DLP-cloud-connector@symantec.com>
2017-02-01 10:36:04 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # the url address of the REST API server
-CDS_LB='https://cbd-e2e2e-cds-1.us-west-2.aws.symantecdlp.com'
+CDS_LB='https://rest-endpoint.example.com'
 # location of client certificate and key
 CDS_CERT='../certs/cds_cert.pem'
 CDS_KEY='../certs/cds_key.pem'
